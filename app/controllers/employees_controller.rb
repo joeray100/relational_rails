ad3fd51784fd208625c08e9c5c7c286c18c60577
Joe Ray <jray100.jr@gmail.com>
2021-03-26 12:10:32 -0600

    Made link to employees on businesses page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,15 +1,19 @@
 class EmployeesController < ApplicationController
 
   def index
-    @business = Business.find(params[:id])
-    @employees = @business.employees.all
+    @employees = Employee.all
   end
 
   def show
-    @employees = Employee.find(params[:id])
+    @business = Business.find(params[:id])
     @employees = @business.employees.count
   end
 
   def new
+  end
+
+  def employee_index
+    @business = Business.find(params[:id])
+    @employees = @business.employees.all
   end
 end
