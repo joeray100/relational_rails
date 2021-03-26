0889a2ea6b16a0680a2d72bcdbe5c3f7340d26f3
Olivia Charles <ocharles92@gmail.com>
2021-03-26 10:39:21 -0400

    Finished Active Record Iteration1

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   get '/businesses', to: 'businesses#index'
   get '/businesses/:id', to: 'businesses#show'
-  get 'businesses/:id/employees', to: 'businesses#employees'
+  get 'businesses/:id/employees', to: 'employees#index'
 
-  get '/employees', to: 'employees#index'
   get '/employees/:id', to: 'employees#show'
 
   get '/bakeries', to: 'bakeries#index'
   get '/bakeries/:id', to: 'bakeries#show'
-  get '/bakeries/:id/pastries', to: 'bakeries#pastries'
+  get '/bakeries/:id/pastries', to: 'pastries#index'
 
-  get '/pastries', to: 'pastries#index'
   get '/pastries/:id', to: 'pastries#show'
 end
--- spec/features/pastries/pastry_show_spec.rb
+++ spec/features/pastries/pastry_show_spec.rb
@@ -8,12 +8,12 @@ describe "Show child with id" do
     cake = bakery1.pastries.create!(name: "Chocolate Cake", price: 4, gluten_free: true)
     bread = bakery1.pastries.create!(name: "Kopf", price: 7, gluten_free: false)
 
-    visit "/pastries/#{cake.id}"
+    visit "/bakeries/#{bakery1.id}/pastries"
     expect(page). to have_content(cake.name)
     expect(page). to have_content(cake.price)
     expect(page). to have_content(cake.gluten_free)
 
-    visit "/pastries/#{bread.id}"
+    visit "/bakeries/#{bakery1.id}/pastries"
     expect(page). to have_content(bread.name)
     expect(page). to have_content(bread.price)
     expect(page). to have_content(bread.gluten_free)
