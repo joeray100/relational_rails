6ba6d8dc839fd97b041b6edfaac4848ba889d664
Olivia Charles <ocharles92@gmail.com>
2021-03-30 18:49:55 -0400

    Finished user story 12 and started 13

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,24 @@ Rails.application.routes.draw do
   get '/businesses/new', to: 'businesses#new'
   get '/businesses/:id', to: 'businesses#show'
   get '/businesses/:id/edit', to: 'businesses#edit'
-  get '/businesses/:id/employees', to: 'business_employees#index'
+  patch '/businesses/:id', to: 'businesses#update'
   post '/businesses', to: 'businesses#create'
+
+  get '/businesses/:id/employees', to: 'business_employees#index'
 
   get '/employees/:id', to: 'employees#show'
   get '/employees', to: 'employees#index'
 
   get '/bakeries/new', to: 'bakeries#new'
+  get '/bakeries/:id/edit', to: 'bakeries#edit'
   get '/bakeries', to: 'bakeries#index'
   get '/bakeries/:id', to: 'bakeries#show'
-  get '/bakeries/:id/edit', to: 'bakeries#edit'
-  get '/bakeries/:id/pastries', to: 'bakery_pastries#index'
+  patch '/bakeries/:id', to: 'bakeries#update'
   post '/bakeries', to: 'bakeries#create'
 
+  get '/bakeries/:id/pastries', to: 'bakery_pastries#index'
+  get '/bakeries/:id/pastries/new', to: 'bakery_pastries#new'
+  post '/bakeries/:id/pastries', to: 'bakery_passries#create'
 
   get '/pastries/:id', to: 'pastries#show'
   get '/pastries', to: 'pastries#index'
---
+++ spec/features/business/edit_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+# Then I see a link to update the parent "Update Parent"
+# When I click the link "Update Parent"
+# Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+# When I fill out the form with updated information
+# And I click the button to submit the form
+# Then a `PATCH` request is sent to '/parents/:id',
+# the parent's info is updated,
+# and I am redirected to the Parent's Show page where I see the parent's updated info
+RSpec.describe 'New Bussiness' do
+  describe 'As a visitor' do
+    describe 'When I visit the businesses index' do
+      it 'I can update a business' do
+        yarn = Business.create!(name: 'Yarn boyz', rank: 3, big_company: true)
+
+        visit "/businesses/#{yarn.id}"
+
+        click_link 'Edit Business'
+
+        expect(current_path).to eq("/businesses/#{yarn.id}/edit")
+
+        fill_in 'Name', with: 'Aaron Carter'
+        fill_in 'Rank', with: 1
+        fill_in 'big_company', with: true
+        click_on 'Update'
+
+        expect(current_path).to eq("/businesses/#{yarn.id}")
+        expect(page).to have_content('Aaron Carter')
+        expect(page).to have_content(1)
+        expect(page).to have_content(true)
+        expect(page).to_not have_content("Yarn Boyz")
+      end
+    end
+  end
+end
