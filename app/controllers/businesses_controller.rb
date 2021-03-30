263aadbbd1af32d316f93c81d9031da17ebc05ec
Joe Ray <jray100.jr@gmail.com>
2021-03-29 19:59:36 -0600

    Update parent controllers/views

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -7,4 +7,30 @@ class BusinessesController < ApplicationController
     @businesses = Business.find(params[:id])
   end
 
+  def new
+    @businesses = Business.new
+  end
+
+  def create
+    @business = Business.new(business_params)
+    @business.save
+
+    redirect_to '/businesses'
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :rank, :big_company)
+  end
+
+  def edit
+    @businesses = Business.all
+  end
+
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+    redirect_to '/businesses'
+  end
 end
--- spec/features/bakery/bakery_new_spec.rb
+++ spec/features/bakery/bakery_new_spec.rb
@@ -1,21 +1,23 @@
+require "rails_helper"
 
-  RSpec.describe 'New Bakery' do
-    describe 'As a visitor' do
-      describe 'When I visit the new new bakery form by clicking a link on the index' do
-        it 'I can create a new bakery' do
-          visit '/bakeries'
+describe "user creates a new bakery" do
+  describe "they link from the bakeries index" do
+    describe "they fill in the appropriate fields" do
+      it "creates a new bakery" do
+        visit bakeries_path
+        click_link "New Bakery"
 
-          click_link 'New Bakery'
+        expect(current_path).to eq('/bakeries/new')
 
-          expect(current_path).to eq('/bakeries/new')
+        fill_in "bakery[name]", with: "New Name"
+        fill_in "bakery[rank]", with: 8
+        fill_in "bakery[open]", with: true
+        click_on "Create Bakery"
 
-          fill_in 'Name', with: 'Jamisons Donuts'
-          fill_in 'Rank', with: 7
-          fill_in 'Open', with: true
-          click_on 'Create Bakery'
 
-          expect(current_path).to eq("/bakeries")
-          expect(page).to have_content('Jamisons Donuts')
+        expect(page).to have_content("New Name")
+        expect(page).to have_content(8)
+        expect(page).to have_content(true)
       end
     end
   end
