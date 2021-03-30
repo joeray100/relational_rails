a18037fe8f8f07baaa2896191683478c6bacb996
Olivia Charles <ocharles92@gmail.com>
2021-03-30 11:11:26 -0400

    Created edit method for update

--- app/controllers/bakeries_controller.rb
+++ app/controllers/bakeries_controller.rb
@@ -1,6 +1,6 @@
 class BakeriesController < ApplicationController
   def index
-    @bakeries = Bakery.order("created_at DESC")
+    @bakeries = Bakery.order(created_at: :desc)
   end
 
   def show
@@ -22,5 +22,22 @@ class BakeriesController < ApplicationController
 
   def bakery_params
     params.require(:bakery).permit(:name, :rank, :open)
+  end
+
+  def edit
+    @bakeries.Bakery.all
+  end
+
+  def update
+    @bakery.Bakery.find(params[:id])
+    @bakery.update
+    @bakery.save
+    redirect_to '/bakeries'
+  end
+
+  def destroy
+    @bakery = Bakery.find(params[:id])
+    @bakery.destroy
+    redirect_to '/bakeries'
   end
 end
